903edb50a771c6b6f275d7ce88dc98fccbbc61cc
laman <lax@gmail.com>
2025-09-25 10:04:48 +0530

    titan commit 25092025

--- src/test/java/Practice/Test25092025.java
+++ src/test/java/Practice/Test25092025.java
@@ -3,7 +3,7 @@ package Practice;
 public class Test25092025 {
 
 	public static void main(String[] args) {
-System.out.println("25.09.2025 hi");
+System.out.println("25.09.2025 hi titian");
 	}
 
 }
