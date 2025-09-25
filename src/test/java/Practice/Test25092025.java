5d4ad61977f54759716d9e2517c92f6c0b51538d
laman <lax@gmail.com>
2025-09-25 10:21:58 +0530

    titan git hiiiiii

--- src/test/java/Practice/Test25092025.java
+++ src/test/java/Practice/Test25092025.java
@@ -3,7 +3,7 @@ package Practice;
 public class Test25092025 {
 
 	public static void main(String[] args) {
-System.out.println("25.09.2025 hi titian git hub");
+System.out.println("25.09.2025 hi titian git hub1");
 	}
 
 }
